8e8b83d9d798e55ed941f76b9088b810b351b180
Ross Mawhorter <rmawhorter@g.hmc.edu>
2019-03-12 17:42:13 -0700

    Preliminary version of pattern parser.

---
+++ world_rando/pattern.py
@@ -0,0 +1,54 @@
+from .room_dtypes import *
+from .coord import *
+
+def parse_pattern(pattern_filename):
+    f = open(pattern_filename, "r")
+    tiles = {}
+    max_tile = Coord(0,0)
+    max_area = 0
+    for y, line in enumerate(f.readlines()):
+        for x, entry in enumerate(line.split()):
+            assert entry[0] == "["
+            assert entry[-1] == "]"
+            entry = entry[1:-1]
+            es = entry.split(",")
+            # Assume BTS
+            if len(es) == 2:
+                bts = 0
+            elif len(es) == 3:
+                bts = int(es[2], 16)
+            else:
+                assert False, "Bad entry: " + entry
+            # Find the texture
+            if es[0] == "_":
+                texture = "ANY"
+            else:
+                tex_index, flips = find_flips(es[0])
+                texture = Texture(tex_index, flips)
+            # Find the type
+            if es[1] == "_":
+                tile_type = "ANY"
+            else:
+                ty_index = int(es[1], 16)
+                ty = Type(ty_index, bts)
+            c = Coord(x, y)
+            a = c.area()
+            tiles[c] = Tile(texture, ty)
+            if a > max_area:
+                max_tile = c
+                max_area = a
+    f.close()
+    level = Level(max_tile + Coord(1,1), tiles=tiles)
+    return level
+
+def find_flips(entry):
+    flips = (0,0)
+    es = entry.split("|")
+    main_entry = int(es[0], 16)
+    if len(es) == 3:
+        hflip = int(es[1])
+        vflip = int(es[2])
+        flips = (hflip, vflip)
+    elif len(es) != 1:
+        assert False, "Bad Entry!"
+    return main_entry, flips
